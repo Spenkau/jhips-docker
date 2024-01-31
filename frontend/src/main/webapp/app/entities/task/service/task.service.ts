1830b3c145ff7fc0694d90a04f508a29c519e11a
Alexander Kholyavski <kholyavskij@mail.ru>
2024-01-31 21:18:03 +0300

    add testing route

--- frontend/src/main/webapp/app/entities/task/service/task.service.ts
+++ frontend/src/main/webapp/app/entities/task/service/task.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
-import { map } from 'rxjs/operators';
+import {map} from 'rxjs/operators';
 
 import dayjs from 'dayjs/esm';
 
-import { isPresent } from 'app/core/util/operators';
-import { DATE_FORMAT } from 'app/config/input.constants';
-import { ApplicationConfigService } from 'app/core/config/application-config.service';
-import { createRequestOption } from 'app/core/request/request-util';
-import { ITask, NewTask } from '../task.model';
+import {isPresent} from 'app/core/util/operators';
+import {DATE_FORMAT} from 'app/config/input.constants';
+import {ApplicationConfigService} from 'app/core/config/application-config.service';
+import {createRequestOption} from 'app/core/request/request-util';
+import {ITask, NewTask} from '../task.model';
 
 export type PartialUpdateTask = Partial<ITask> & Pick<ITask, 'id'>;
 
@@ -31,11 +31,23 @@ export type EntityArrayResponseType = HttpResponse<ITask[]>;
 @Injectable({ providedIn: 'root' })
 export class TaskService {
   protected resourceUrl = this.applicationConfigService.getEndpointFor('api/tasks');
+  protected resourceUrl2 = this.applicationConfigService.getEndpointFor('api/test');
 
   constructor(
     protected http: HttpClient,
     protected applicationConfigService: ApplicationConfigService,
   ) {}
+
+  test(): Observable<any> {
+    return this.http.get(this.resourceUrl2, {observe: 'response'})
+      .pipe(map((res) => this.convertDateFromServer2(res)));
+  }
+
+  protected convertDateFromServer2(restTest: any): ITask {
+    return {
+      ...restTest
+    };
+  }
 
   create(task: NewTask): Observable<EntityResponseType> {
     const copy = this.convertDateFromClient(task);
