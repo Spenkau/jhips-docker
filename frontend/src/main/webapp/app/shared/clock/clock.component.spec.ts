bf10e95d9f84c9c4fb7793d87a56bfd8e1d5b3e6
Alexander Kholyavski <kholyavskij@mail.ru>
2024-02-04 21:43:56 +0300

    removing clock component

--- frontend/src/main/webapp/app/shared/clock/clock.component.spec.ts
+++ frontend/src/main/webapp/app/shared/clock/clock.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ClockComponent } from './clock.component';
+import {ClockComponent} from './clock.component';
 
 describe('ClockComponent', () => {
   let component: ClockComponent;
@@ -11,7 +11,7 @@ describe('ClockComponent', () => {
       imports: [ClockComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(ClockComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
